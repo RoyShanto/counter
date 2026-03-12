f4af38595745acc25e4230b2e60de1f76bee5f6c
Shanto Roy <roycse17@gmail.com>
2026-03-12 19:18:45 +0600

    clean version

---
+++ demoData.js
@@ -0,0 +1,22 @@
+export const demoData = [
+    { label: '39?45?898*', p: "U", startDate: '2026-02-19', closeDate: '2031-02-19', policy: "sameDate", periods: 60, interval: 1 },
+    { label: '30?15?375', p: "U", startDate: '2026-02-04', closeDate: '2026-05-04', policy: "sameDate", periods: 1, interval: 3 },
+    { label: '27?10?646*', p: "O", startDate: '2025-04-28', closeDate: '2028-04-28', policy: "sameDate", periods: 12, interval: 3 },
+    { label: '27?10?562*', p: "O", startDate: '2025-05-04', closeDate: '2028-05-04', policy: "sameDate", periods: 12, interval: 3 },
+    { label: '26?10?482*', p: "O", startDate: '2025-08-21', closeDate: '2028-08-21', policy: "sameDate", periods: 12, interval: 3 },
+    { label: '10?5?406', p: "O", startDate: '2025-01-04', closeDate: '2026-04-04', policy: "sameDate", periods: 5, interval: 3 },
+    { label: '10?5?406', p: "O", startDate: '2025-01-04', closeDate: '2026-04-04', policy: "sameDate", periods: 5, interval: 3 },
+    { label: '10?5?406', p: "O", startDate: '2025-01-04', closeDate: '2026-04-04', policy: "sameDate", periods: 5, interval: 3 },
+    { label: '3?5?562', p: "O", startDate: '2025-11-19', closeDate: '2026-05-19', policy: "monthEnd", periods: 6, interval: 1 },
+    { label: '3?5?562', p: "O", startDate: '2025-11-19', closeDate: '2026-05-19', policy: "monthEnd", periods: 6, interval: 1 },
+    { label: '3?5?562', p: "O", startDate: '2025-11-19', closeDate: '2026-05-19', policy: "monthEnd", periods: 6, interval: 1 },
+    { label: '3?5?562', p: "O", startDate: '2025-11-19', closeDate: '2026-05-19', policy: "monthEnd", periods: 6, interval: 1 },
+    { label: '13?5?837*', p: "N", startDate: '2025-05-15', closeDate: '2028-05-15', policy: "sameDate", periods: 12, interval: 3 },
+    { label: '13?5?269*', p: "N", startDate: '2025-08-17', closeDate: '2028-08-17', policy: "sameDate", periods: 12, interval: 3 },
+    { label: '52?20?965*', p: "N", startDate: '2025-09-29', closeDate: '2028-09-29', policy: "sameDate", periods: 12, interval: 3 },
+    { label: '3?5?562', p: "N", startDate: '2026-01-27', closeDate: '2027-01-27', policy: "monthEnd", periods: 12, interval: 1 },
+    { label: '3?5?562', p: "N", startDate: '2026-01-27', closeDate: '2027-01-27', policy: "monthEnd", periods: 12, interval: 1 },
+    { label: '3?5?562', p: "N", startDate: '2026-01-27', closeDate: '2027-01-27', policy: "monthEnd", periods: 12, interval: 1 },
+    { label: '2?3?137', p: "N", startDate: '2026-03-08', closeDate: '2027-03-08', policy: "monthEnd", periods: 12, interval: 1 },
+    { label: '1?2.5?781', p: "N", startDate: '2026-01-28', closeDate: '2027-01-27', policy: "monthEnd", periods: 12, interval: 1 }
+];
